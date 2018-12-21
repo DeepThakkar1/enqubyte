fb7b0753e4298c784f69084b0c158335ea58c703
Trumpets <operations@trumpetstechnologies.com>
2018-12-21 15:29:13 +0530

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,25 +3,9 @@
 @section('content')
 
 
-
 <div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+    <h2 class="d-inline-block headline-content">Dashboard</h2>
+    <hr>
 </div>
 @include('components.demo.demomodal')
 @endsection
