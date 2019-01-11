0f426d8a773312c2f5dd57d88247a21419204ec7
Trumpets <operations@trumpetstechnologies.com>
2019-01-11 18:01:52 +0530

    add tabs in reports

---
+++ resources/views/statements/partials/tabs.blade.php
@@ -0,0 +1,8 @@
+<ul class="nav nav-pills d-flex justify-content-center mb-3 mt-3 " id="pills-tab" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" id="pills-tabular-tab" data-toggle="pill" href="#pills-tabular" role="tab" aria-controls="pills-tabular" aria-selected="false">Tabular</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " id="pills-chart-tab" data-toggle="pill" href="#pills-chart" role="tab" aria-controls="pills-chart" aria-selected="true">Chart</a>
+    </li>
+</ul>
--- resources/views/statements/profit_loss_account.blade.php
+++ resources/views/statements/profit_loss_account.blade.php
@@ -9,71 +9,63 @@
             <i class="fa fa-angle-right ml-2 mr-2" aria-hidden="true"></i> Statements
             <i class="fa fa-angle-right ml-2 mr-2" aria-hidden="true"></i> Profit & Loss Account
         </h2>
-    </div>
-
-    <div class="card mt-4">
-        <div class="card-body">
-            <div class="d-flex justify-content-around align-items-center p-3">
-                <div class="px-4">
-                    <h3 class="">Net Profit </h3>
-                    <h4 class="">&#8377; {{$profit}}</h4>
+        @include('statements.partials.filter')
+        @include('statements.partials.tabs')
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-tabular" role="tabpanel" aria-labelledby="pills-tabular-tab">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-around align-items-center p-3">
+                            <div class="px-4">
+                                <h3 class="">Net Profit </h3>
+                                <h4 class="">&#8377; {{$profit}}</h4>
+                            </div>
+                            <div class="px-4">
+                                <h4 class="">=</h4>
+                            </div>
+                            <div class="px-4">
+                                <h3 class="">Total sale</h3>
+                                <h4 class="">&#8377; {{$totalSale}}</h4>
+                            </div>
+                            <div class="px-4">
+                                <h4 class="">-</h4>
+                            </div>
+                            <div class="px-4">
+                                <h3 class="">Expenses</h3>
+                                <h4 class="">&#8377; {{$expenses}}</h4>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="px-4">
-                    <h4 class="">=</h4>
-                </div>
-                <div class="px-4">
-                    <h3 class="">Total sale</h3>
-                    <h4 class="">&#8377; {{$totalSale}}</h4>
-                </div>
-                <div class="px-4">
-                    <h4 class="">-</h4>
-                </div>
-                <div class="px-4">
-                    <h3 class="">Expenses</h3>
-                    <h4 class="">&#8377; {{$expenses}}</h4>
-                </div>
+                <table class="table">
+                    <tbody>
+                        <tr class="bg-light">
+                            <th colspan="2">Expenses</th>
+                        </tr>
+                        <tr>
+                            <td>Total Purchase Made</td>
+                            <td>&#8377; {{$totalPurchase}}</td>
+                        </tr>
+                        <tr>
+                            <td>Incentives Paid</td>
+                            <td>&#8377; {{$incentives}}</td>
+                        </tr>
+                        <tr class="bg-light">
+                            <th colspan="2">Income</th>
+                        </tr>
+                        <tr>
+                            <td>Total Sale</td>
+                            <td>&#8377; {{$totalSale}}</td>
+                        </tr>
+                        <tr class="bg-light">
+                            <th>Net Profit</th>
+                            <th>&#8377; {{$profit}}</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="tab-pane fade" id="pills-chart" role="tabpanel" aria-labelledby="pills-chart-tab">
             </div>
         </div>
-    </div>
-    <ul class="nav nav-pills d-flex justify-content-center mb-3 mt-3 " id="pills-tab" role="tablist">
-        <li class="nav-item">
-            <a class="nav-link active" id="pills-tabular-tab" data-toggle="pill" href="#pills-tabular" role="tab" aria-controls="pills-tabular" aria-selected="false">Tabular</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link " id="pills-chart-tab" data-toggle="pill" href="#pills-chart" role="tab" aria-controls="pills-chart" aria-selected="true">Chart</a>
-        </li>
-    </ul>
-    <div class="tab-content" id="pills-tabContent">
-        <div class="tab-pane fade show active" id="pills-tabular" role="tabpanel" aria-labelledby="pills-tabular-tab">
-        <table class="table">
-            <tbody>
-                <tr class="bg-light">
-                    <th colspan="2">Expenses</th>
-                </tr>
-                <tr>
-                    <td>Total Purchase Made</td>
-                    <td>&#8377; {{$totalPurchase}}</td>
-                </tr>
-                <tr>
-                    <td>Incentives Paid</td>
-                    <td>&#8377; {{$incentives}}</td>
-                </tr>
-                <tr class="bg-light">
-                    <th colspan="2">Income</th>
-                </tr>
-                <tr>
-                    <td>Total Sale</td>
-                    <td>&#8377; {{$totalSale}}</td>
-                </tr>
-                <tr class="bg-light">
-                    <th>Net Profit</th>
-                    <th>&#8377; {{$profit}}</th>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="tab-pane fade" id="pills-chart" role="tabpanel" aria-labelledby="pills-chart-tab">
-    </div>
-</div>
 
-@endsection
+        @endsection
