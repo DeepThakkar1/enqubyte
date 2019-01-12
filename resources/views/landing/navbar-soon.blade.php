54dd270f3ce94775e26cdc9a3ecf50f091eec4eb
Trumpets Technologies <work.trumpets@gmail.com>
2019-01-12 09:40:37 +0530

    UI changes

---
+++ resources/views/landing/navbar-soon.blade.php
@@ -0,0 +1,84 @@
+    
+   {{-- <header class="xs-header header-transparent header-style3">
+        <div class="container">
+            <nav class="xs-menus clearfix">
+                <div class="nav-header header-logo">
+                    <a class="nav-brand" href="/">
+                        <img src="/img/enque bite colour white.png" alt="">
+                    </a>
+                    <div class="nav-toggle"></div>
+                </div>
+                <div class="nav-menus-wrapper align-to-right">
+                   <!--  <ul class="nav-menu">
+                        <li><a href="/index">Home</a>
+                        </li>
+                        <li><a href="/feature" style="color: #fff !important;">Features</a>
+                        </li>
+                    </ul> -->
+                    <ul class="xs-menu-tools">
+                       <!--  <li>
+                            <a href="#modal-popup-2" class="navsearch-button xs-modal-popup"><i class="icon icon-search"></i></a>
+                        </li> -->
+                        <li>
+                            <a href="#" class="navSidebar-button"><i class="icon icon-burger-menu" style="font-size: 30px;"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div><!-- .container END -->
+    </header><!-- End header section --> --}}
+<header class="xs-header header-transparent header-style3 desktop-navbar">
+    <div class="header-transparent nav-sticky nav-lights" data-scroll-to="top">
+    <div class="off-canvas-menu-area munu-style-border">
+        <div class="container">
+            <div class="offset-menu-wraper clearfix">
+                <div class="float-left">
+                    <a href="/" class="offcanvas-nav-brand">
+                        <img src="/img/logo.png" alt="" style="height: 30px;margin-top: 5px;">
+                    </a>
+                </div>
+               <!--  <div class="float-right">
+                    <div class="offsetmenu-btn humburger fullscreen_menu_tigger clearfix">
+                        <div class="humburger-icons">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div> -->
+                <div class="nav-menus-wrapper align-to-right custom-nav-content">
+                    <ul class="nav-menu " style="padding-right: 0px;">
+                        
+                        <li>
+                            <a href="#" class="nav-link btn btn-primary custom-register-btn text-white px-4 ml-2">Coming Soon</a>
+                        </li>
+                    </ul>                
+                </div>
+            </div>
+        </div>
+    </div>    <!-- End header section -->
+</div>
+</header>
+
+<header class="xs-header header-transparent nav-lights responsive-navbar">
+    <div class="container" style="box-shadow: 0 2px 4px rgba(0, 0, 0, 0.04);">
+        <nav class="xs-menus clearfix">
+            <div class="nav-header">
+                <a class="nav-brand" href="index-2.html">
+                    <img src="/img/logo.png" alt="" style="width: 133px;margin-left: -14px;">
+                </a>
+                <div class="nav-toggle"></div>
+            </div>
+            <div class="nav-menus-wrapper align-to-right">
+                <!-- menu list -->
+                <ul class="nav-menu">
+                   
+                    <li style="padding: 10px;">
+                        <a href="#" class="btn-primary custom-register-btn text-white" style="padding: 10px;">Coming Soon</a>
+                    </li>
+                </ul>                
+            </div>
+            
+        </nav>
+    </div><!-- .container END -->
+</header><!-- End header section -->
