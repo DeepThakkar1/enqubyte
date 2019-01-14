69763e1b5643a8d8495f15e7618e12148b30ad8c
Trumpets Technologies <work.trumpets@gmail.com>
2019-01-14 10:01:36 +0530

    UI changes

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -41,7 +41,23 @@
     
 	<body>
         
+		
+		<!-- Load Facebook SDK for JavaScript -->
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
 
+		<!-- Your customer chat code -->
+		<div class="fb-customerchat"
+		  attribution=install_email
+		  page_id="359699814831143">
+		</div>
+		
 	   <main id="content">
             @yield('content')
         </main>
