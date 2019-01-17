54d65ea142affb14d0709eb31398ec7425532b48
Trumpets <operations@trumpetstechnologies.com>
2019-01-17 16:59:32 +0530

    enquiry pdf

--- resources/views/settings/tabs/general.blade.php
+++ resources/views/settings/tabs/general.blade.php
@@ -36,24 +36,24 @@
                     <input type="checkbox" name="weekly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->weekly ? 'checked' : ''}} value="1" id="chkWeekly"> Weekly
                 </label> -->
                 <div class="custom-control custom-checkbox d-inline-block">
-                  <input type="checkbox" class="custom-control-input" id="chkWeekly">
-                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkWeekly" name="weekly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->weekly ? 'checked' : ''}} value="1" id="chkWeekly"> Weekly </label>
+                  <input type="checkbox" class="custom-control-input"  name="weekly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->weekly ? 'checked' : ''}} value="1" id="chkWeekly">
+                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkWeekly"> Weekly </label>
                 </div>
                 <div class="custom-control custom-checkbox d-inline-block">
-                  <input type="checkbox" class="custom-control-input" id="chkMonthly">
-                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkMonthly" name="monthly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->monthly ? 'checked' : ''}} value="1" id="chkMonthly"> Monthly </label>
+                  <input type="checkbox" class="custom-control-input" name="monthly" id="chkMonthly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->monthly ? 'checked' : ''}} value="1">
+                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkMonthly" > Monthly </label>
                 </div>
                 <div class="custom-control custom-checkbox d-inline-block">
-                  <input type="checkbox" class="custom-control-input" id="chkQuarterly">
-                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkQuarterly"  name="quarterly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->quarterly ? 'checked' : ''}} value="1" id="chkQuarterly"> Quarterly </label>
+                  <input type="checkbox" class="custom-control-input"  name="quarterly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->quarterly ? 'checked' : ''}} value="1" id="chkQuarterly">
+                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkQuarterly"> Quarterly </label>
                 </div>
                 <div class="custom-control custom-checkbox d-inline-block">
-                  <input type="checkbox" class="custom-control-input" id="chkSixMonth">
-                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkSixMonth"  name="sixmonth" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->sixmonth ? 'checked' : ''}} value="1" id="chkSixMonth"> Six Month </label>
+                  <input type="checkbox" class="custom-control-input"  name="sixmonth" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->sixmonth ? 'checked' : ''}} value="1" id="chkSixMonth">
+                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkSixMonth"> Six Month </label>
                 </div>
                 <div class="custom-control custom-checkbox d-inline-block">
-                  <input type="checkbox" class="custom-control-input" id="chkYearly">
-                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkYearly"  name="yearly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->yearly ? 'checked' : ''}} value="1" id="chkYearly" > Yearly </label>
+                  <input type="checkbox" class="custom-control-input" id="chkYearly"  name="yearly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->yearly ? 'checked' : ''}} value="1" id="chkYearly">
+                  <label class="custom-control-label pt-1 pr-3 pl-1" for="chkYearly"> Yearly </label>
                 </div>
                 <!-- <label class="mr-4" for="chkMonthly">
                     <input type="checkbox" name="monthly" {{isset(auth()->user()->reportfrequency) && auth()->user()->reportfrequency->monthly ? 'checked' : ''}} value="1" id="chkMonthly"> Monthly
