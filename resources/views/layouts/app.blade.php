79c18f93b474c04ac954739f354092a7d9c6b5b0
Trumpets <operations@trumpetstechnologies.com>
2019-01-19 13:26:07 +0530

    table responsive issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -138,19 +138,7 @@
                 rowReorder: {
                     selector: 'td:nth-child(2)'
                 },
-                responsive: {
-                    breakpoints: [
-                        {name: 'bigdesktop', width: Infinity},
-                        {name: 'meddesktop', width: 1480},
-                        {name: 'smalldesktop', width: 1280},
-                        {name: 'medium', width: 1188},
-                        {name: 'tabletl', width: 1024},
-                        {name: 'btwtabllandp', width: 848},
-                        {name: 'tabletp', width: 768},
-                        {name: 'mobilel', width: 480},
-                        {name: 'mobilep', width: 320}
-                    ]
-                },
+                // responsive: true,
                 searching: false,
                 paging: false
             });
@@ -159,19 +147,7 @@
                 rowReorder: {
                     selector: 'td:nth-child(2)'
                 },
-                responsive: {
-                    breakpoints: [
-                        {name: 'bigdesktop', width: Infinity},
-                        {name: 'meddesktop', width: 1480},
-                        {name: 'smalldesktop', width: 1280},
-                        {name: 'medium', width: 1188},
-                        {name: 'tabletl', width: 1024},
-                        {name: 'btwtabllandp', width: 848},
-                        {name: 'tabletp', width: 768},
-                        {name: 'mobilel', width: 480},
-                        {name: 'mobilep', width: 320}
-                    ]
-                },
+                // responsive: true,
                 "order": [[ 0, "desc" ]],
                 searching: false,
                 paging: false
