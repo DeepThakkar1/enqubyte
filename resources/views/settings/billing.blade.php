b69f9c70ff80f0105e19e5a1566f014c8ce3397b
Abhishek Wani <work.trumpets@gmail.com>
2019-01-19 15:32:04 +0530

    billing page

--- resources/views/settings/billing.blade.php
+++ resources/views/settings/billing.blade.php
@@ -53,7 +53,7 @@
 		            <div class="d-flex">
 		                <div class="px-md-4">
 		                	<h3 class="text-size-heading font-weight-bold">INV-00{{ $transaction->id }}</h3>
-		                	<div style="font-size: 20px;">For the month of {{ $transaction->created_at->locale('en')->monthName }}</div>
+		                	<div style="font-size: 20px;">For the month of {{ $transaction->created_at->toFormattedDateString() }}</div>
 		                </div>	
 			            <div class="ml-auto p-2">
 	                        <a href="/payments/invoice/{{$transaction->id}}" style="font-size: 20px;" class="btn btn-outline-success">Download Invoice</a>
