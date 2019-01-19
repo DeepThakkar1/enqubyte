078e983842a633de4d028fa7acb68c2b3da7256c
Trumpets <operations@trumpetstechnologies.com>
2019-01-19 17:07:32 +0530

    dashboard changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,7 +70,7 @@
         <div class="col-xl-5 col-sm-5 mb-3">
             <ul class="list-group">
                 <li class="list-group-item bg-light d-flex justify-content-between align-items-center">
-                    <h4 class="m-0">Today's Followups</h4>
+                    <h5 class="m-0">Today's Followups</h5>
                 </li>
                 @foreach($followups as $followup)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -85,8 +85,11 @@
         </div>
         <div class="col-xl-7 col-sm-7 mb-3">
             <div class="card">
+                <div class="card-header">
+                    <h5 class="m-0">Today's Followups</h5>
+                </div>
                 <div class="card-body">
-                    <canvas id="myAreaChart" width="100%" height="30"></canvas>
+                    <canvas id="dashboardBarChart" width="100%"></canvas>
                 </div>
             </div>
         </div>
@@ -99,59 +102,38 @@
 <script src="{{asset('js/Chart.bundle.js')}}"></script>
 <script src="{{asset('js/utils.js')}}"></script>
 <script>
-        // Set new default font family and font color to mimic Bootstrap's default styling
-        Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-        Chart.defaults.global.defaultFontColor = '#292b2c';
+var color = Chart.helpers.color;
+    var config = {
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: [
+                        {{$cancelledEnqCnt}},
+                        {{$convertedEnqCnt}},
+                        {{$pendingEnqCnt}},
+                    ],
+                    backgroundColor: [
+                        window.chartColors.red,
+                        window.chartColors.green,
+                        window.chartColors.yellow,
+                    ],
+                    label: 'Dataset 1'
+                }],
+                labels: [
+                    'Cancelled Enquiries',
+                    'Converted Enquiries',
+                    'Pending Enquiries'
+                ]
+            },
+            options: {
+                responsive: true
+            }
+        };
 
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
-    datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 50,
-      pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-  }],
-},
-options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-      },
-      gridLines: {
-          display: false
-      },
-      ticks: {
-          maxTicksLimit: 7
-      }
-  }],
-  yAxes: [{
-    ticks: {
-      min: 0,
-      max: 40000,
-      maxTicksLimit: 5
-  },
-  gridLines: {
-      color: "rgba(0, 0, 0, .125)",
-  }
-}],
-},
-legend: {
-  display: false
-}
-}
-});
+        window.onload = function() {
+            var ctx = document.getElementById('dashboardBarChart').getContext('2d');
+            window.myPie = new Chart(ctx, config);
+        };
+
 </script>
 @endpush
