0c4fd5c7445b86d0787c591cd82dc21a253b9c31
Abhishek Wani <work.trumpets@gmail.com>
2019-01-20 23:39:45 +0530

    subdomain testing

--- resources/views/visitors/show.blade.php
+++ resources/views/visitors/show.blade.php
@@ -40,7 +40,7 @@
         </div>
     </div>   
 
-    <div class="container px-md-5">
+    <div class="container px-md-5 mt-4">
         <div class="card">
             <div class="card-header">
                 <strong>Enquiries</strong>
