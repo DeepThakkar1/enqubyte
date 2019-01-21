8081275333afee78dfb55021e12928a03dc5922b
Abhishek Wani <work.trumpets@gmail.com>
2019-01-21 14:22:41 +0530

    billing

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -28,11 +28,16 @@ class PaymentsController extends Controller
     public function success($paymentId = null, $requestId = null)
     {
     	$details = Mojo::giveMePaymentDetails();
-    	dd($details);
-    	$plan = PlanModel::where('name', 'All-in-one monthly')->first();
-    	$subscription = auth()->user()->subscribeTo($plan, 30); // 30 days
+    	if($details->payment->status == 'Credit')
+    	{
+    			$plan = PlanModel::where('name', 'All-in-one monthly')->first();
+    			$subscription = auth()->user()->subscribeTo($plan, 30); // 30 days
 		
-    	return redirect('subscribed');
+    			return redirect('subscribed');
+    	} else {
+    		return redirect('billing');
+    	}
+    
 
     }
 
