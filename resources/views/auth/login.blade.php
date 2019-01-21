dcc67be9196cd0ee60416b79c1bd707e5e9ea7bf
Trumpets Technologies <work.trumpets@gmail.com>
2019-01-21 13:21:00 +0530

    UI changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,8 +22,8 @@
                 <a href="/">
                     <img src="{{ url('img/logo.png') }}" height="60px">
                 </a>
-                <div style="width:90%; margin-top: 17vh;">
-                <h1 class="mb-5 mt-4">{{ __('Sign in to your account') }}</h1>
+                <div class="user-signin-account">
+                <h1 class="mb-5 mt-md-4">{{ __('Sign in to your account') }}</h1>
                 <div class="">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
