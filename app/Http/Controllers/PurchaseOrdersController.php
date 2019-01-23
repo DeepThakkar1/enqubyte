87012e67c9378826209112b398f89166cc5434d6
Trumpets <operations@trumpetstechnologies.com>
2019-01-23 09:51:26 +0530

    tax store

--- app/Http/Controllers/PurchaseOrdersController.php
+++ app/Http/Controllers/PurchaseOrdersController.php
@@ -70,6 +70,14 @@ class PurchaseOrdersController extends Controller
 
         $purchaseSrno =PurchaseOrder::orderBy('created_at', 'desc')->where('company_id', auth()->id())->count() + 1;
 
+        $taxes = [];
+        if (request()->has('tax_amt') && count(request('tax_amt'))) {
+            foreach (request('tax_amt') as $key => $tax_amt) {
+                $taxes[] = [request('tax_abbrivation')[$key] => $tax_amt];
+            }
+        }
+
+
         $purchaseOrder = PurchaseOrder::create([
             'company_id' => auth()->id(),
             'sr_no' => $purchaseSrno,
@@ -84,6 +92,7 @@ class PurchaseOrdersController extends Controller
             'discount' => !empty(request('discount')) ? request('discount') : 0,*/
             'grand_total' => request('grand_total'),
             'remaining_amount' => request('grand_total'),
+            'taxes' => json_encode($taxes),
             'order_scan_copy' => $scanCopy
         ]);
         for ($i=0; $i < count(request('product_id')); $i++) {
--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 class PurchaseOrder extends Model
 {
     protected $fillable = [
-        'sr_no', 'company_id', 'store_id', 'order_id', 'vendor_id', 'due_date', 'purchase_date', 'sub_tot_amt', 'grand_total', 'order_scan_copy', 'remaining_amount', 'status', 'discount', 'discount_type'
+        'sr_no', 'company_id', 'store_id', 'order_id', 'vendor_id', 'due_date', 'purchase_date', 'sub_tot_amt', 'grand_total', 'order_scan_copy', 'remaining_amount', 'status', 'discount', 'discount_type', 'taxes'
     ];
 
     public function company()
@@ -38,5 +38,10 @@ class PurchaseOrder extends Model
     public function payments()
     {
         return $this->hasMany(PurchaseOrderRecordPayment::class);
+    }
+
+    public function getTaxesAttribute()
+    {
+        return json_decode($this->attributes['taxes']);
     }
 }
